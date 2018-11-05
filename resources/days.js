595987def7cd342691df088fb93f4aab2dbc0ae3
fyum <fyulheri@gmail.com>
2018-11-05 22:18:05 +0000

    add days

---
+++ resources/days.js
@@ -0,0 +1,40 @@
+const DAYS = [
+  {
+    name: 'Monday',
+    shortName: 'Mon',
+    number: 1,
+  },
+  {
+    name: 'Tuesday',
+    shortName: 'Tue',
+    number: 2,
+  },
+  {
+    name: 'Wednesday',
+    shortName: 'Wed',
+    number: 3,
+  },
+  {
+    name: 'Thursday',
+    shortName: 'Thu',
+    number: 4,
+  },
+  {
+    name: 'Friday',
+    shortName: 'Fri',
+    number: 5,
+  },
+  {
+    name: 'Saturday',
+    shortName: 'Sat',
+    number: 6,
+  },
+  {
+    name: 'Sunday',
+    shortName: 'Sun',
+    number: 7,
+  },
+];
+
+export default
+DAYS;
